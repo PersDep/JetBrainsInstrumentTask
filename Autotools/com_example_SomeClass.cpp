11e2addbac224dc542ab133c92fc33f1ead96f3a
Persdep <ilyanapar@yandex.ru>
2017-06-05 21:57:53 +0300

    Fixed exception

--- Autotools/com_example_SomeClass.cpp
+++ Autotools/com_example_SomeClass.cpp
@@ -1,6 +1,14 @@
+// #ifndef _JAVASOFT_JNI_H_
+// #error "_JAVASOFT_JNI_H_ not defined; using a non-JavaSoft JDK!"
+// #endif
+
+// #ifdef _CLASSPATH_JNI_H
+// #error "Using GNU Classpath!"
+// #endif
+
 #include "com_example_SomeClass.h"
 
 JNIEXPORT void JNICALL Java_com_1example_1SomeClass_test
   (JNIEnv *env, jclass clazz) {
-	env->FatalError("Some java exception");
+    env->ThrowNew(clazz, "Some java exception xxx");
 }
